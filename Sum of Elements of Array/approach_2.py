c65aea44ead7a271b07496c476d745223d3e474f
Anamika <72334601+noviicee@users.noreply.github.com>
2021-05-16 23:46:58 +0530

    Update approach_2.py

--- Sum of Elements of Array/approach_2.py
+++ Sum of Elements of Array/approach_2.py
@@ -1,14 +1,15 @@
-def FindSumInSubarray(arr,num):
-    d={}
+def FindSumInSubarray(arr, num):
+    d = {}
     # x+y=10
     # y=10-x
     for x in arr:
 
-        d[x]=num-x
+        d[x] = num - x
     for i in d.values():
         if i in arr:
-            return i,d[i]
+            return i, d[i]
     return None
+
 
 """Note:
 This program takes into account that there is only one such subarray
@@ -19,9 +20,9 @@ In such a case, we need to store the values instead of returning single value he
 """
 
 
-if __name__=="__main__":
-    print(FindSumInSubarray([4,3,-1,17,5,11,18,13],10))
+if __name__ == "__main__":
+    print(FindSumInSubarray([4, 3, -1, 17, 5, 11, 18, 13], 10))
     print()
-    print(FindSumInSubarray([-4,-1,1,3,5,6,8,11],10))
+    print(FindSumInSubarray([-4, -1, 1, 3, 5, 6, 8, 11], 10))
     print()
-    print(FindSumInSubarray([1,2,3,4,5,6,71,8],20))
+    print(FindSumInSubarray([1, 2, 3, 4, 5, 6, 71, 8], 20))
